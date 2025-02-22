f2d3953954c57944333908366a5b03c9927a92aa
flvffywvffy <marcus@marcusleonasharvey.com>
2025-02-22 22:35:17 +0000

    add terms page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="text-xl font-semibold">Terms of Service</h1>
+      <p>By using status.flvffy.top, you agree to these terms.</p>
+
+      <ul className="list-inside list-disc pt-4">
+        <li>No pornographic content</li>
+        <li>No racist, bigoted, or other hatespeach in your status.</li>
+        <li>No harrasing, bullying, or picking on other users.</li>
+        <li>No revealing information in your status.</li>
+      </ul>
+    </section>
+  );
+}
