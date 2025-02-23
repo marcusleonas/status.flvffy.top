83639a977f777e04dc88a3ba6b485dfe99e99948
flvffywvffy <marcus@marcusleonasharvey.com>
2025-02-23 21:04:31 +0000

    update schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -26,7 +26,7 @@ export const status = createTable("status", {
 export const user = createTable("user", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
-  username: text("username"),
+  username: text("username").unique(),
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").notNull(),
   image: text("image"),
